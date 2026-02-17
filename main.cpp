99def78f43e2b0ec393a3f22a8b40d36970e9b45
StarukhDaniil <starukh.danil@gmail.com>
2026-02-17 18:05:56 +0200

    feat: add jsoncpp dependency via vcpkg

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include "json/json.h"
 #include "logger.hpp"
 
 int main() {
-    say_hello();
+    Json::Value root;
+    root["name"] = "Daniil";
+
+    Json::StreamWriterBuilder builder;
+    const std::string json_file = Json::writeString(builder, root);
+
+    std::cout << json_file << std::endl;
+
     return 0;
 }
