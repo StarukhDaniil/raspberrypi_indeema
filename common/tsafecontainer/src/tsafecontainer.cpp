dcd485f1a0736bf31746ad83368ebba41d61991e
StarukhDaniil <starukh.danil@gmail.com>
2026-02-24 14:35:23 +0200

    feat: add tsafecontainer class

---
+++ common/tsafecontainer/src/tsafecontainer.cpp
@@ -0,0 +1,50 @@
+#include "tsafecontainer.hpp"
+
+template <typename T>
+const std::shared_ptr<T>& TSafeContainer<T>::container() const {
+    return m_conteiner_ptr;
+}
+
+template <typename T>
+const std::shared_ptr<std::condition_variable> TSafeContainer<T>::cv() const {
+    return m_cv_ptr;
+}
+
+template <typename T>
+const std::shared_ptr<std::shared_mutex> TSafeContainer<T>::mtx() const {
+    return m_mtx_ptr;
+}
+
+template <typename T>
+const std::function<bool(void)>& TSafeContainer<T>::cv_check_func() const {
+    return m_cv_check_func;
+}
+
+template <typename T>
+std::shared_ptr<T>& TSafeContainer<T>::container() {
+    return m_conteiner_ptr;
+}
+
+template <typename T>
+std::shared_ptr<std::condition_variable>& TSafeContainer<T>::cv() {
+    return m_cv_ptr;
+}
+
+template <typename T>
+std::shared_ptr<std::shared_mutex>& TSafeContainer<T>::mtx() {
+    return m_mtx_ptr
+}
+
+template <typename T>
+std::function<bool(void)>& TSafeContainer<T>::cv_check_func() {
+    return m_cv_check_func;
+}
+
+template <typename T>
+TSafeContainer<T>::TSafeContainer(const std::shared_ptr<T>& container,
+                   const std::shared_ptr<std::condition_variable>& cv,
+                   const std::shared_ptr<std::shared_mutex>& mtx,
+                   const std::function<bool(void)> cv_check) {
+
+}
+
